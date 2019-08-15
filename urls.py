20c4ca1a55cb73d048efdcb6ee7a7c7d818680d3
EY-X <e.yosefx128@gmail.com>
2019-08-14 20:38:00 -0400

    dotenv not working

--- urls.py
+++ urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from pokegifs.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('pokemon/<int:id>', pokemon_show),
 ]
